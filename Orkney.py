bc4352f40d667dc8b79653645f6ab9d797c67b9c
Chae Gordon <chae.gordon@lakeparime.com>
2020-01-21 10:51:28 +0000

    add some data & update interpolation to have more extremes

---
+++ Orkney.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 21 10:22:24 2020
+
+@author: PeterParker
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_excel("10min 2019 export.xls")
--- interpolation.py
+++ interpolation.py
@@ -11,6 +11,8 @@ import seaborn as sns
 
 df = pd.read_csv("T1.csv")
 
+sigma = 0.03
+
 # want to take the 10 minute power data and interpolate into 1-minute data
 
 # normalising data into fractional values of total rated power
@@ -24,12 +26,17 @@ interpolated = []
 # if 30 minute data change 10--> 30
 # energy should remain unaffected
 
-for i in range(len(power)-1):
+# not linearly interpolating j*(power[i+1]-power[i])/10 +
+# beacause its a sumulative average
+# + j*(power[i+1]-power[i])/10
+
+for i in range(len(power)-9):
     for j in range(10):
         if 10 > j > 0:
-            a = power[i] + j*(power[i+1]-power[i])/10 + np.random.normal(loc=0,scale=0.05)
+            # have it so random fluctuation is centred on previous point
+            a = power[i+j] + np.random.normal(loc=0,scale=sigma)
         else:
-            a = power[i] + j*(power[i+1]-power[i])/10
+            a = power[i] 
         if a > 0:
             if 1 > a:
                 interpolated.append(a)
@@ -39,11 +46,30 @@ for i in range(len(power)-1):
             interpolated.append(0)
     
 
-power = power[0:3]
-interpolated = interpolated[0:21]
+power_g = power[0:3]
+interpolated_g = interpolated[0:21]
 
 plt.figure()
-plt.plot(np.linspace(0, len(interpolated),num=len(interpolated),endpoint=False), interpolated)
+plt.plot(np.linspace(0, len(interpolated_g),num=len(interpolated_g),endpoint=False), interpolated_g)
 #plt.figure()
-plt.scatter(np.linspace(0, 10*len(power),num=len(power),endpoint=False), power)
+plt.scatter(np.linspace(0, 10*len(power_g),num=len(power_g),endpoint=False), power_g)
 plt.show()
+
+# checking energy conservation (want a plot!)
+# energy
+
+energy_interpolated = sum(interpolated)*(1/60)*df["LV ActivePower (kW)"].max()  # kWh
+energy_data = (1/6)*sum(power)*df["LV ActivePower (kW)"].max()  # kWh
+
+print((energy_interpolated-energy_data)/(np.average(power)*df["LV ActivePower (kW)"].max()))
+
+# 44 hours in 365 days
+
+# print(100*(2/365))  # 0.54%
+
+# Want E cons. as a function of time
+
+energy_t_interpolated = []
+energy_t_data = []
+
+# want to count loss
